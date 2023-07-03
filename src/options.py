16029f48d64c4b53b35e1c9fcce77917303ac8eb
Almaz <79905215+almazgimaev@users.noreply.github.com>
2023-07-03 16:06:10 +0300

    Update options.py

--- src/options.py
+++ src/options.py
@@ -60,8 +60,8 @@ CLASSES_PREVIEW_GAP: int = 10
 ### * Set up visualization params for ClassesHeatmaps class ###
 # args for "to_image" method
 DRAW_STYLE: str = None  # "inside_white" or "outside_black"
-HEATMAP_ROWS: int = None
-HEATMAP_COLS: int = None
+HEATMAP_ROWS: int = 1
+HEATMAP_COLS: int = 4
 HEATMAP_GRID_SPACING: int = None
 HEATMAP_OUTER_GRID_SPACING: int = None
 HEATMAP_OUTPUT_WIDTH: int = None # 1 class in dataset? -> 1600px for portrait images, 2200px for landscape
